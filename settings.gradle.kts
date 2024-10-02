cd450227b737771c21a37a59dc475bf312f8b886
Joe Groocock <me@frebib.net>
2024-10-02 12:12:33 +0000

    Quieten gradle build
    Signed-off-by: Joe Groocock <me@frebib.net>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -78,7 +78,6 @@ fun includeProjects(directory: File, path: String, maxDepth: Int = 1) {
             val buildFile = File(file, "build.gradle.kts")
             if (buildFile.exists()) {
                 include(newPath)
-                logger.lifecycle("Included project: $newPath")
             } else if (maxDepth > 0) {
                 includeProjects(file, newPath, maxDepth - 1)
             }
